81bee77b3d95b60954af66aa6c2f762203ff9e20
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-05-03 21:03:21 +0900

    make number of unit to variable

--- build.sh
+++ build.sh
@@ -29,6 +29,19 @@ echo -e "> '$build_type' is selected"
 echo -e ""
 
 
+echo -en "Q. What is the number of units? : "
+read _unit_n
+if [[ "$_unit_n" =~ ^[0-9]+$ ]]; then
+    unit_n=$_unit_n
+else
+    echo -e "> '$_unit_type' is invalid for the number of units."
+    unit_n=3
+fi
+
+echo -e "> the number of units is '$unit_n'"
+echo -e ""
+
 cmake -S . -B _build \
     -D CMAKE_BUILD_TYPE=${build_type} \
+    -D MINE_UNIT_N=${unit_n} \
 && cmake --build _build
--- include/mine/const.hpp
+++ include/mine/const.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
 // TODO: should make selectable on compiling
+#if defined(MINE_UNIT_N)
+const static int N = MINE_UNIT_N;
+#else
 const static int N = 3;
+#endif
 const static double PI = 3.14159265358979323846264338;
